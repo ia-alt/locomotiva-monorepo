ae9338cefd99cae9bada8fcbed3b5580f37d6bda
IA <ia@secti2.ma.gov.br>
2026-03-31 21:27:37 -0300

    url in .env(mobile and tablet)

--- apps/tablet/src/locomotiva-api/link.ts
+++ apps/tablet/src/locomotiva-api/link.ts
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 export const LS_API_KEY = 'locomotiva_tablet_api_key'
 
 export const link = new RPCLink({
-    url: 'http://localhost:3000/api',
+    url: process.env.EXPO_PUBLIC_API_URL!,
     headers: async () => {
         const apiKey = await AsyncStorage.getItem(LS_API_KEY)
         return apiKey ? { 'x-api-key': apiKey } : {}
